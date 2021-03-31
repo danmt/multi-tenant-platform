393d3785699e8cfb8a8c70ccca03a3e11e08fc3c
Daniel Marin <danielarturomt@gmail.com>
2021-03-31 11:45:10 -0300

    feat: Set up executor for emulator (#9) closes #5

--- apps/first-tenant/src/environments/environment.prod.ts
+++ apps/first-tenant/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyAaPK6LAFovTtoVedoI-O3XFRb4igcr3FU',
+    authDomain: 'first-tenant.firebaseapp.com',
+    projectId: 'first-tenant',
+    storageBucket: 'first-tenant.appspot.com',
+    messagingSenderId: '539831515976',
+    appId: '1:539831515976:web:23093cc8056c6690c2917d',
+  },
 };
--- apps/second-tenant/src/environments/environment.prod.ts
+++ apps/second-tenant/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3RPmMOxbgAUeyUykSGVXUZ8XWVbYxPCA',
+    authDomain: 'second-tenant.firebaseapp.com',
+    projectId: 'second-tenant',
+    storageBucket: 'second-tenant.appspot.com',
+    messagingSenderId: '654490908069',
+    appId: '1:654490908069:web:44f398da1580de88c7244c',
+  },
 };
